538b11073008a98b39fa16cec2c35817deacb83c
murshid_pc <murshidpc@gmail.com>
2021-03-10 18:02:20 +0530

    completed the monsters project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.components';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => { 
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+    .catch(err => {
+      console.log(err);
+    })
+  },[])
+
+  const filteredUsers = users.filter((monster => monster.name.toLowerCase().includes(search)));
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox placeholder="search monsters" handleChange={handleChange} />
+      <CardList monsters={filteredUsers} />
     </div>
   );
 }
---
+++ src/components/cards/card.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './card.style.css';
+
+
+export const Card = (props) => {
+    return(
+        <div className='card-container'>
+            <img alt="monsters" width="180px" height="180px" src={`https://robohash.org/${props.monster.id}{?set=set2`} />
+            <h2>{props.monster.name}</h2>
+        </div>
+    )
+}
